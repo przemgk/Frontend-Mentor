ed69e3b11d24775f50f4fc91678cb6cbe8f9b1e1
przemgk <przem_gk@o2.pl>
2019-08-28 01:02:21 +0200

    validation completed

---
+++ 02/js/scripts.js
@@ -0,0 +1,20 @@
+var form = document.querySelector(".section__notify"),
+    emailInput = form.querySelector(".form__email"),
+    emailSendButton = form.querySelector(".form__send"),
+    errorMessageContainer = form.querySelector(".form__message--error"),
+    regex = /^[a-z0-9\._-]+@\w+\.(\w{2,4}(\.\w{2,4})?)$/i;
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+
+    if (regex.test(emailInput.value) === true) {
+        emailInput.classList.remove("form__input--error");
+        errorMessageContainer.style.display = "none";
+
+        emailSendButton.setAttribute("disabled", "disabled");
+        emailSendButton.value = "Success!";
+    } else {
+        emailInput.classList.add("form__input--error");
+        errorMessageContainer.style.display = "block";
+    }
+}
