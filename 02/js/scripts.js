3540d48da7431a25d6257eab08afa6b74d3c82fd
przemgk <przem_gk@o2.pl>
2019-08-28 21:40:03 +0200

    final version

--- 02/js/scripts.js
+++ 02/js/scripts.js
@@ -2,7 +2,7 @@ var form = document.querySelector(".section__notify"),
     emailInput = form.querySelector(".form__email"),
     emailSendButton = form.querySelector(".form__send"),
     errorMessageContainer = form.querySelector(".form__message--error"),
-    regex = /^[a-z0-9\._-]+@\w+\.(\w{2,4}(\.\w{2,4})?)$/i;
+    regex = /^[a-z0-9._-]+@\w+\.(\w{2,4}(\.\w{2,4})?)$/i;
 
 form.onsubmit = function (e) {
     e.preventDefault();
@@ -17,4 +17,4 @@ form.onsubmit = function (e) {
         emailInput.classList.add("form__input--error");
         errorMessageContainer.style.display = "block";
     }
-}
+};
