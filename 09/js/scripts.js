a251cf80ed3d2ebb4a2ed73bfe137a078c040983
przemgk <przem_gk@o2.pl>
2019-09-17 23:40:09 +0200

    add mobile menu

--- 09/js/scripts.js
+++ 09/js/scripts.js
@@ -1,5 +1,7 @@
 const hamburgerIcon = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 
 hamburgerIcon.addEventListener("click", () => {
     hamburgerIcon.classList.toggle("hamburger--active");
+    menu.classList.toggle("menu--open");
 });
