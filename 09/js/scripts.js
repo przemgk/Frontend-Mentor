9c66a8346dd3a0407124693a35bf1fa81ff265d4
przemgk <przem_gk@o2.pl>
2019-09-18 00:19:06 +0200

    finish hamburger interactions

--- 09/js/scripts.js
+++ 09/js/scripts.js
@@ -5,3 +5,12 @@ hamburgerIcon.addEventListener("click", () => {
     hamburgerIcon.classList.toggle("hamburger--active");
     menu.classList.toggle("menu--open");
 });
+
+document.addEventListener("click", e => {
+    const className = e.target.className;
+
+    if (!(className.includes("hamburger") || className.includes("menu"))) {
+        hamburgerIcon.classList.remove("hamburger--active");
+        menu.classList.remove("menu--open");
+    }
+});
