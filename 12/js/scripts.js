34744824cf7cececc17ac42bf0a7fcc921051b39
przemgk <przem_gk@o2.pl>
2019-09-29 01:40:27 +0200

    hamburger menu animations

--- 12/js/scripts.js
+++ 12/js/scripts.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
+const header = document.querySelector(".header");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger--active");
+    header.classList.toggle("header--opened");
 });
