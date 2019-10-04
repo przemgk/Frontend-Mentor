b1523be3bab493732df20754c2ef9c3a76a9f6b0
przemgk <przem_gk@o2.pl>
2019-10-04 17:10:09 +0200

    turn off hamburger menu

--- 12/js/scripts.js
+++ 12/js/scripts.js
@@ -1,13 +1,12 @@
 // Hamburger menu - test version
-
 const hamburger = document.querySelector(".hamburger");
 const navContainer = document.querySelector(".nav-container");
 const logo = document.querySelector(".header .logo");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger--active");
-    navContainer.classList.toggle("nav-container--opened");
-    logo.classList.toggle("logo--white");
+    // navContainer.classList.toggle("nav-container--opened");
+    // logo.classList.toggle("logo--white");
 });
 
 // Carousel
