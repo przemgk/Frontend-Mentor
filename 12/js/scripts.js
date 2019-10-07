3e69d92f170c083e59339bbece942aa76d4d042b
przemgk <przem_gk@o2.pl>
2019-10-08 00:27:43 +0200

    toggle for test version and start again for performance

--- 12/js/scripts.js
+++ 12/js/scripts.js
@@ -1,37 +1,49 @@
+// // Hamburger menu
+// const hamburger = document.querySelector(".hamburger");
+// const header = document.querySelector(".header");
+
+// hamburger.addEventListener("click", () => {
+
+//     hamburger.classList.toggle("hamburger--active");
+//     header.classList.toggle("header--opened");
+
+// } );
+
+// // Carousel
+
+// // FAQ Accordion
+// const questions = Array.from(document.querySelectorAll(".questions__item"));
+
+// questions.forEach( node => {
+
+//     const label =  node.querySelector(".questions__label");
+//     const maxHeight = getComputedStyle(node).height;
+//     const transition = getComputedStyle(node).transition;
+
+//     node.style.maxHeight = getComputedStyle(label).height;
+
+//     label.addEventListener("click", () => {
+
+//         node.classList.toggle("questions__item--active");
+
+//         if(node.classList.contains("questions__item--active")) {
+//             node.style.maxHeight = maxHeight;
+//         } else {
+//             node.style.maxHeight = getComputedStyle(label).height;
+//         }
+
+//     });
+
+// } );
+
+
 // Hamburger menu
-const hamburger = document.querySelector(".hamburger");
-const header = document.querySelector(".header");
+const hamburgerIcon = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
 
-hamburger.addEventListener("click", () => {
+navigation.addEventListener("transitionend", () => navigation.classList.remove("navigation--animatable"));
 
-    hamburger.classList.toggle("hamburger--active");
-    header.classList.toggle("header--opened");
-
-} );
-
-// Carousel
-
-// FAQ Accordion
-const questions = Array.from(document.querySelectorAll(".questions__item"));
-
-questions.forEach( node => {
-
-    const label =  node.querySelector(".questions__label");
-    const maxHeight = getComputedStyle(node).height;
-    const transition = getComputedStyle(node).transition;
-
-    node.style.maxHeight = getComputedStyle(label).height;
-
-    label.addEventListener("click", () => {
-
-        node.classList.toggle("questions__item--active");
-
-        if(node.classList.contains("questions__item--active")) {
-            node.style.maxHeight = maxHeight;
-        } else {
-            node.style.maxHeight = getComputedStyle(label).height;
-        }
-
-    });
-
-} );
+hamburgerIcon.addEventListener("click", () => {
+    navigation.classList.add("navigation--animatable");
+    navigation.classList.toggle("navigation--opened");
+});
