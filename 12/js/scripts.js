fef2c103134c285764930032a1c12aacb9336b69
przemgk <przem_gk@o2.pl>
2019-10-11 23:42:19 +0200

    fix carousel

--- 12/js/scripts.js
+++ 12/js/scripts.js
@@ -9,7 +9,6 @@ hamburgerIcon.addEventListener("click", () => {
     navigation.classList.toggle("navigation--opened");
 });
 
-
 // Carousel
 const carouselTabs = document.querySelectorAll(".carousel__tab");
 const carouselWrapper = document.querySelector(".carousel__wrapper");
@@ -68,24 +67,59 @@ carouselTabs.forEach( (tab, index) => tab.addEventListener("click", e => {
 // FAQ Accordion
 const questions = document.querySelectorAll(".questions__item");
 
-questions.forEach( elem => {
+window.onload = () => {
 
-    const label =  elem.querySelector(".questions__label");
-    const maxHeight = getComputedStyle(elem).height;
-    const transition = getComputedStyle(elem).transition;
+    questions.forEach( elem => {
+        const label =  elem.querySelector(".questions__label");
+        const labelHeight = `${label.getBoundingClientRect().height}px`;
+        const maxHeight = `${elem.getBoundingClientRect().height}px`;
 
-    elem.style.maxHeight = getComputedStyle(label).height;
+        elem.style.maxHeight = labelHeight;
 
-    label.addEventListener("click", () => {
+        label.addEventListener("click", () => {
 
-        elem.classList.toggle("questions__item--active");
+            elem.classList.toggle("questions__item--active");
 
-        if(elem.classList.contains("questions__item--active")) {
-            elem.style.maxHeight = maxHeight;
-        } else {
-            elem.style.maxHeight = getComputedStyle(label).height;
-        }
-
+            if(elem.classList.contains("questions__item--active")) {
+                elem.style.maxHeight = maxHeight;
+            } else {
+                elem.style.maxHeight = labelHeight;
+            }
+        });
     });
+
+};
+
+
+
+
+// Form Validation
+const form = document.querySelector(".form");
+const emailInput = form.querySelector(".form__input");
+const message = form.querySelector(".form__message");
+const buttonSubmit = form.querySelector("button[type='submit'");
+const regexp = /^[a-z0-9_\-.]+@\w+\.\w{2,4}((\.\w{2,4})?)$/gi;
+
+buttonSubmit.style.transform = `translateY(-${Number.parseInt(getComputedStyle(message).height) + Number.parseInt(getComputedStyle(message).marginTop)}px)`;
+
+form.addEventListener("submit", e => {
+
+    e.preventDefault();
+    message.style.transform = "scale(1)"
+    buttonSubmit.style.transform = "none";
+
+    if(regexp.test(emailInput.value)) {
+        emailInput.classList.remove("from__message--error");
+        message.style.transform = "scale(0)"
+        buttonSubmit.style.transform = `translateY(-${Number.parseInt(getComputedStyle(message).height) + Number.parseInt(getComputedStyle(message).marginTop)}px)`;
+
+        buttonSubmit.setAttribute("disabled", "disabled");
+        buttonSubmit.textContent = "Success!";
+    } else {
+        emailInput.classList.add("from__message--error");
+        message.style.transform = "scale(1)"
+        buttonSubmit.style.transform = "none";
+    }
+
 
 } );
