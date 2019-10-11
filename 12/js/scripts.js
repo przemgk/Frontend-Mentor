b9b001416dc263d0f293f390fde605f865b68fed
przemgk <przem_gk@o2.pl>
2019-10-11 15:34:43 +0200

    carousel

--- 12/js/scripts.js
+++ 12/js/scripts.js
@@ -50,3 +50,63 @@ hamburgerIcon.addEventListener("click", () => {
 
 
 // Carousel
+const carouselTabs = document.querySelectorAll(".carousel__tab");
+const carouselWrapper = document.querySelector(".carousel__wrapper");
+const indicator = document.querySelector(".carousel__indicator");
+
+let tabsHeights = [];
+let currentIndex = 0;
+
+// Set indicator position
+function setIndicator(index, indicatorRef) {
+    let height = 0;
+
+    for(let i = 0; i <= index; i++) {
+        height += tabsHeights[i];
+    }
+
+    height -= indicatorRef.getBoundingClientRect().height + Number.parseInt(getComputedStyle(carouselTabs[index]).borderBottomWidth);
+
+    indicatorRef.style.transform = `translateY(${height}px)`;
+}
+
+// Calculate heights of tabs
+function calculateTabsHeights(tabs, tabsHeightsRef) {
+    const heights = [];
+    tabs.forEach( tab => heights.push(tab.getBoundingClientRect().height) );
+
+    return heights;
+}
+
+window.onload = () => {
+    tabsHeights = calculateTabsHeights(carouselTabs);
+    setIndicator(0, indicator);
+};
+window.onresize = () => {
+    tabsHeights = calculateTabsHeights(carouselTabs);
+    setIndicator(0, indicator);
+};
+
+
+carouselTabs.forEach( (tab, index) => {
+    tab.addEventListener("click", e => {
+
+        e.preventDefault();
+
+        if(currentIndex != index) {
+            // Change tabs styles
+            carouselTabs[currentIndex].classList.remove("carousel__tab--active");
+            tab.classList.add("carousel__tab--active");
+
+            // Move indicator
+            setIndicator(index, indicator);
+
+            // Move items
+            carouselWrapper.style.transform = `translateX(-${index * 100}%)`;
+
+            // Change current index
+            currentIndex = index;
+        }
+    });
+
+});
